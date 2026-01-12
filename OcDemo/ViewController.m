76b7dffd242225e5356b7115d02e3d7923fdc9b9
陈伟强 <15875006020@qq.com>
2026-01-12 18:15:30 +0800

    初始化项目

--- OcDemo/ViewController.m
+++ OcDemo/ViewController.m
@@ -15,7 +15,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [button setTitle:@"点我" forState:UIControlStateNormal];
+    button.translatesAutoresizingMaskIntoConstraints = NO;
+    [button addTarget:self action:@selector(homeButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+
+    [NSLayoutConstraint activateConstraints:@[
+        [button.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+        [button.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor],
+    ]];
+}
+
+- (void)homeButtonTapped:(UIButton *)sender {
+    NSLog(@"首页按钮被点击");
 }
 
 
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '11.0'
+project 'OcDemo.xcodeproj'
+
+target 'OcDemo' do
+end
