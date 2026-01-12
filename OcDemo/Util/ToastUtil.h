9f79df44bb20d6d925c253f9ec9956bc842bcb2e
陈伟强 <15875006020@qq.com>
2026-01-12 19:23:03 +0800

    全局吐司

---
+++ OcDemo/Util/ToastUtil.h
@@ -0,0 +1,12 @@
+//
+//  ToastUtil.h
+//  OcDemo
+//
+//  Created by cwq on 2026/1/12.
+//
+
+#ifndef ToastUtil_h
+#define ToastUtil_h
+
+
+#endif /* ToastUtil_h */
---
+++ OcDemo/Util/ToastUtil.m
@@ -0,0 +1,8 @@
+//
+//  ToastUtil.m
+//  OcDemo
+//
+//  Created by cwq on 2026/1/12.
+//
+
+#import <Foundation/Foundation.h>
