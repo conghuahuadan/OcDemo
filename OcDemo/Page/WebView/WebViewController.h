67674dac264e172094d3a21858773bcdbb867c2d
陈伟强 <15875006020@qq.com>
2026-01-12 19:33:04 +0800

    webview示例

---
+++ OcDemo/Page/WebView/WebViewController.h
@@ -0,0 +1,16 @@
+//
+//  WebViewController.h
+//  OcDemo
+//
+//  Created by cwq on 2026/1/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WebViewController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OcDemo/Page/WebView/WebViewController.m
@@ -0,0 +1,12 @@
+//
+//  WebViewController.m
+//  OcDemo
+//
+//  Created by cwq on 2026/1/12.
+//
+
+#import "WebViewController.h"
+
+@implementation WebViewController
+
+@end
