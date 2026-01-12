008c22c713da3e0d58263ad6f230def1f93a228d
陈伟强 <15875006020@qq.com>
2026-01-13 00:42:04 +0800

    滚动截图

---
+++ OcDemo/Page/WebView/WebViewPrintPageRenderer.h
@@ -0,0 +1,16 @@
+//
+//  WebViewPrintPageRenderer.h
+//  OcDemo
+//
+//  Created by cwq on 2026/1/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WebViewPrintPageRenderer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OcDemo/Page/WebView/WebViewPrintPageRenderer.m
@@ -0,0 +1,12 @@
+//
+//  WebViewPrintPageRenderer.m
+//  OcDemo
+//
+//  Created by cwq on 2026/1/13.
+//
+
+#import "WebViewPrintPageRenderer.h"
+
+@implementation WebViewPrintPageRenderer
+
+@end
